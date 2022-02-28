fc824e9376386f6d676b86f4fe8831ad6c95c322
Al Siam <mdmahedialsiam@gmail.com>
2022-02-28 15:13:31 +0600

    updated contact sectioin

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,17 +1,21 @@
-import "./contact.scss"
+import "./contact.scss";
 
 export default function Contact() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
         <div className="contact" id="contact">
             <div className="left">
-                <img src="assets/myImg.png" alt="" />
+                <h1>Let's discuss about the project</h1>
             </div>
             <div className="right">
                 <h2>Contact.</h2>
-                <form>
+                <form onSubmit={handleSubmit} >
                     <input type="text" placeholder="Email" />
                     <textarea placeholder="Message"></textarea>
-                    <button type="submit">Send</button>
+                    <button type="submit" >Send</button>
                 </form>
             </div>
         </div>
